27aa911e67008bc1c107c4d29a222237656211b1
aufgosh <42834590+aufgosh@users.noreply.github.com>
2024-04-22 22:11:24 +0200

    spelling fixes

--- AntiDragonFlightInArena.lua
+++ AntiDragonFlightInArena.lua
@@ -1,14 +1,14 @@
 local function OnEvent(self, event, ...)
 	local mapID = C_Map.GetBestMapForUnit("player")
 	if
-	GetZoneText() == "Ruins of Lordaeron" or GetZoneText() == "Nokhudon Proving Grounds" or GetZoneText() == "Dalaran Arena" or GetZoneText() == "Tol'Viron Arena" or GetZoneText() == "Tiger's Peak" or
-	GetZoneText() == "Ashamane's Fall" or GetZoneText() == "Black Rock Hold Arena" or GetZoneText() == "Hook Point" or GetZoneText() == "The Mugambala" or GetZoneText() == "The Robodrome" or
-	GetZoneText() == "Empyrean Domain" or GetZoneText() == "Maldraxxus Coliseum" or GetZoneText() == "Enigma Crucible" or GetZoneText() == "Blade's Edge Arena" or
-	 then
+	GetZoneText() == "Ruins of Lordaeron" or GetZoneText() == "Nokhudon Proving Grounds" or GetZoneText() == "Dalaran Arena" or GetZoneText() == "Tol'Viron Arena" or GetZoneText() == "The Tiger's Peak" or
+	GetZoneText() == "Ashamane's Fall" or GetZoneText() == "Black Rook Hold Arena" or GetZoneText() == "Hook Point" or GetZoneText() == "The Mugambala" or GetZoneText() == "The Robodrome" or
+	GetZoneText() == "Empyrean Domain" or GetZoneText() == "Maldraxxus Coliseum" or GetZoneText() == "Enigma Crucible" or GetZoneText() == "Blade's Edge Arena" or GetZoneText() == "Nagrand Arena"
+	then
 	UIWidgetPowerBarContainerFrame:Hide()
 	else
 	UIWidgetPowerBarContainerFrame:Show()
-	  end
+	end
 end
 
 local f = CreateFrame("Frame")
